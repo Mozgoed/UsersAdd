41163372da1bdade386538104a2ed92736efe5e2
hukimato <mozgoed123@mail.ru>
2018-11-29 16:53:19 +0300

    Работает через файл

--- UsersAdd/Form1.Designer.cs
+++ UsersAdd/Form1.Designer.cs
@@ -28,13 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.radFile = new System.Windows.Forms.RadioButton();
+            this.radText = new System.Windows.Forms.RadioButton();
+            this.txtUsers = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtDepartments = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(207, 23);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(300, 26);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(513, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(86, 32);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Обзор";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // radFile
+            // 
+            this.radFile.AutoSize = true;
+            this.radFile.Location = new System.Drawing.Point(12, 24);
+            this.radFile.Name = "radFile";
+            this.radFile.Size = new System.Drawing.Size(189, 24);
+            this.radFile.TabIndex = 3;
+            this.radFile.Text = "Загрузить из файла";
+            this.radFile.UseVisualStyleBackColor = true;
+            // 
+            // radText
+            // 
+            this.radText.AutoSize = true;
+            this.radText.Checked = true;
+            this.radText.Location = new System.Drawing.Point(12, 54);
+            this.radText.Name = "radText";
+            this.radText.Size = new System.Drawing.Size(262, 24);
+            this.radText.TabIndex = 4;
+            this.radText.TabStop = true;
+            this.radText.Text = "Загрузить из текстового окна";
+            this.radText.UseVisualStyleBackColor = true;
+            // 
+            // txtUsers
+            // 
+            this.txtUsers.Location = new System.Drawing.Point(12, 84);
+            this.txtUsers.Multiline = true;
+            this.txtUsers.Name = "txtUsers";
+            this.txtUsers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUsers.Size = new System.Drawing.Size(587, 260);
+            this.txtUsers.TabIndex = 5;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(393, 382);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(206, 32);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Записать в файл";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 382);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(148, 32);
+            this.btnGenerate.TabIndex = 7;
+            this.btnGenerate.Text = "Сгенерировать";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(239, 382);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(148, 32);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "Скопировать";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt файлы|*.txt|Все файлы|*.*";
+            this.openFileDialog1.Title = "Выберите файл с пользователями";
+            // 
+            // txtDepartments
+            // 
+            this.txtDepartments.Location = new System.Drawing.Point(151, 350);
+            this.txtDepartments.Name = "txtDepartments";
+            this.txtDepartments.Size = new System.Drawing.Size(448, 26);
+            this.txtDepartments.TabIndex = 9;
+            this.txtDepartments.Text = ",OU=10 класс,OU=Ученики,DC=informatics,DC=school272";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Подразделения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(611, 539);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDepartments);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtUsers);
+            this.Controls.Add(this.radText);
+            this.Controls.Add(this.radFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "UsersAdd .Net by Mozgoed";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RadioButton radFile;
+        private System.Windows.Forms.RadioButton radText;
+        private System.Windows.Forms.TextBox txtUsers;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtDepartments;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- UsersAdd/Form1.cs
+++ UsersAdd/Form1.cs
@@ -14,5 +14,112 @@ namespace UsersAdd
         {
             InitializeComponent();
         }
+
+        static string ConvertWord2Translit(string word)
+        {
+            word = word.ToLower();
+            string newWord = "";
+            for (int i = 0; i < word.Length; i++)
+                newWord += ConvertChar2Translit(word[i]);
+            return newWord;
+        }
+
+        static string ConvertChar2Translit(char c)
+        {
+            string[] rus = new string[33];
+            string[] eng = new string[33];
+            rus[0] = "а"; eng[0] = "a";
+            rus[1] = "б"; eng[1] = "b";
+            rus[2] = "в"; eng[2] = "v";
+            rus[3] = "г"; eng[3] = "g";
+            rus[4] = "д"; eng[4] = "d";
+            rus[5] = "е"; eng[5] = "e";
+            rus[6] = "ё"; eng[6] = "yo";
+            rus[7] = "ж"; eng[7] = "g";
+            rus[8] = "з"; eng[8] = "z";
+            rus[9] = "и"; eng[9] = "i";
+            rus[10] = "й"; eng[10] = "y";
+            rus[11] = "к"; eng[11] = "k";
+            rus[12] = "л"; eng[12] = "l";
+            rus[13] = "м"; eng[13] = "m";
+            rus[14] = "н"; eng[14] = "n";
+            rus[15] = "о"; eng[15] = "o";
+            rus[16] = "п"; eng[16] = "p";
+            rus[17] = "р"; eng[17] = "r";
+            rus[18] = "с"; eng[18] = "s";
+            rus[19] = "т"; eng[19] = "t";
+            rus[20] = "у"; eng[20] = "u";
+            rus[21] = "ф"; eng[21] = "f";
+            rus[22] = "х"; eng[22] = "h";
+            rus[23] = "ц"; eng[23] = "c";
+            rus[24] = "ч"; eng[24] = "ch";
+            rus[25] = "ш"; eng[25] = "w";
+            rus[26] = "щ"; eng[26] = "w";
+            rus[27] = "ь"; eng[27] = "";
+            rus[28] = "ъ"; eng[28] = "";
+            rus[29] = "ы"; eng[29] = "i";
+            rus[30] = "э"; eng[30] = "e";
+            rus[31] = "ю"; eng[31] = "yu";
+            rus[32] = "я"; eng[32] = "ya";
+            for (byte n = 0; n < rus.Length; n++)
+                if (rus[n][0] == c) return eng[n];
+            return c.ToString();
+        }
+        string result = string.Empty;
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (radText.Checked)
+            {
+                char[] splitchar = { ' ' };
+                foreach (string s in txtUsers.Text.Split(splitchar,StringSplitOptions.RemoveEmptyEntries))
+                {
+
+                }
+            }
+            if (radFile.Checked)
+            {
+                if(!System.IO.File.Exists(txtFile.Text))
+                {
+                    MessageBox.Show("Файл не найден");
+                    return;
+                }
+                System.IO.StreamReader read = new System.IO.StreamReader(txtFile.Text);
+                System.IO.StreamWriter write = new System.IO.StreamWriter("users.bat", true, Encoding.GetEncoding(866));
+                string fullname = read.ReadLine();
+                while (fullname != null)
+                {
+                    string[] separator = { " " };
+                    fullname = fullname.Trim();
+                    if (fullname == "") { fullname = read.ReadLine(); continue; }
+                    string[] pieces = fullname.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                    fullname = pieces[0] + " " + pieces[1];
+                    Console.WriteLine(fullname);
+                    string firstName = ConvertWord2Translit(pieces[0]);
+                    string lastName = ConvertWord2Translit(pieces[1]);
+                    string profileName = firstName + "." + lastName;
+                    Console.WriteLine(profileName);
+                    Console.WriteLine();
+                    write.WriteLine("dssadd user \"cn={0}{1}\" -display \"{0}\" -samid {2} -upn {2} -profile \\\\inf0\\Prifiles$\\{2} -pwd {3} -ln {3} -fn {4} -rewersiblepwn yes -mustchpwd yes",
+                                                    fullname,txtDepartments,profileName,lastName,firstName);
+                    fullname = read.ReadLine();
+                    System.Threading.Thread.Sleep(200);
+                }
+                write.Flush(); write.Close();
+                read.Close();
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                txtFile.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(result);
+        }
     }
 }
